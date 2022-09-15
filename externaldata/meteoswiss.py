af4aec68777ef4ebde67d16856c080fe5b08db58
JamesRunnalls <james.runnalls@eawag.ch>
2022-09-15 10:27:24 +0200

    Edit location path

--- externaldata/meteoswiss.py
+++ externaldata/meteoswiss.py
@@ -2,8 +2,13 @@ import os
 from .functions import logger
 
 
-def cosmo_1(folder):
+def cosmo_1(data_folder):
     log = logger("cosmo_1", path=os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "logs"))
-    log.initialise("Downloading COSMO 1 data")
+    log.initialise("Processing COSMO 1 data")
+    log.info("Ensure data folder exists.")
+    folder = os.path.join(data_folder, "meteoswiss/cosmo1")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    log.info("Downloading data file...")
     with open(os.path.join(folder, "cosmo_file.txt"), "a") as file:
         file.write("COSMO data" + "\n")
