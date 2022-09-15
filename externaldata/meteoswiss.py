314d994bc919347e1b88bb28eaa91e3bc38d08ec
JamesRunnalls <james.runnalls@eawag.ch>
2022-09-15 12:38:47 +0200

    Add parameters

--- externaldata/meteoswiss.py
+++ externaldata/meteoswiss.py
@@ -2,7 +2,7 @@ import os
 from .functions import logger
 
 
-def cosmo_1(data_folder):
+def cosmo_1(data_folder, ftp_password, ftp_host="sftp.eawag.ch", ftp_user="cosmo"):
     log = logger("cosmo_1", path=os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "logs"))
     log.initialise("Processing COSMO 1 data")
     log.info("Ensure data folder exists.")
--- externaldata/runs/meteoswiss_cosmo1.py
+++ externaldata/runs/meteoswiss_cosmo1.py
@@ -2,7 +2,7 @@ import os
 import sys
 from externaldata.meteoswiss import cosmo_1
 
-if len(sys.argv) == 2 and os.path.exists(str(sys.argv[1])):
-    cosmo_1(str(sys.argv[1]))
+if len(sys.argv) == 3 and os.path.exists(str(sys.argv[1])):
+    cosmo_1(str(sys.argv[1]), str(sys.argv[2]))
 else:
-    raise ValueError("Download directory not provided.")
+    raise ValueError("Download directory and/or ftp password not provided.")
