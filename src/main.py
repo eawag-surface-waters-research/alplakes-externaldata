a0ce761fad5358a2ca845524fe43e10b7fa8e879
James Runnalls <runnalls.james@gmail.com>
2023-10-26 18:22:02 +0200

    Add Meteoswiss meteo data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import argparse
-from meteoswiss import cosmo
+from meteoswiss import cosmo, meteodata
 from bafu import hydrodata
 
 
@@ -9,17 +9,19 @@ def main(params):
     setups = ["meteoswiss_cosmo", "bafu_hydrodata"]
     if params["source"] == "meteoswiss_cosmo":
         cosmo(params["filesystem"], params["password"])
+    elif params["source"] == "meteoswiss_meteodata":
+        meteodata(params["filesystem"], params["password"])
     elif params["source"] == "bafu_hydrodata":
         hydrodata(params["filesystem"], params["key"])
     else:
-        raise Exception("Currently only the following simulations are supported: {}".format(setups))
+        raise Exception("Currently only the following sources are supported: {}".format(setups))
 
 
 if __name__ == "__main__":
     if sys.version_info[0:2] != (3, 9):
         raise Exception('Requires python 3.9')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source', '-s', help="Data source [meteoswiss_cosmo, bafu_hydrodata]", type=str)
+    parser.add_argument('--source', '-s', help="Data source [meteoswiss_cosmo, bafu_hydrodata, meteoswiss_meteodata]", type=str)
     parser.add_argument('--filesystem', '-f', help="Path to local storage filesystem", type=str,)
     parser.add_argument('--password', '-p', help="FTP password", type=str, default=False)
     parser.add_argument('--key', '-k', help="Path to ssh key file", type=str, default=False)
--- src/meteoswiss.py
+++ src/meteoswiss.py
@@ -1,6 +1,7 @@
 import os
 import pysftp
 import fnmatch
+import pandas as pd
 from functions import logger, unzip_combine, progressbar
 
 
@@ -70,3 +71,73 @@ def cosmo(data_folder, ftp_password, ftp_host="sftp.eawag.ch", ftp_port=22, ftp_
 
     if len(failed) > 0:
         raise ValueError("Failed to download: {}".format(", ".join(failed)))
+
+
+def meteodata(data_folder, ftp_password, folder="data", ftp_host="sftp.eawag.ch", ftp_port=22, ftp_user="simstrat"):
+    """
+    Download Meteodata from Eawag sftp server.
+    A single file for the previous day is made available at around 10:15am and contains hourly data for a number of stations.
+    This function looks for any non downloaded dates and process these files.
+    """
+    failed = []
+
+    log = logger("meteodata", path=os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "logs"))
+    log.initialise("Download Meteodata from Eawag sftp server")
+
+    log.info("Ensure data folder exists.")
+    parent = os.path.join(data_folder, "meteoswiss/meteodata")
+    if not os.path.exists(parent):
+        os.makedirs(parent)
+
+    log.info("Connecting to {}".format(ftp_host))
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    conn = pysftp.Connection(host=ftp_host, port=ftp_port, username=ftp_user, password=ftp_password, cnopts=cnopts)
+    log.info("Successfully connected to {}".format(ftp_host))
+
+    server_files = conn.listdir(folder)
+    server_files.sort()
+    last_update_file = os.path.join(parent, "last_update.txt")
+
+    if os.path.exists(last_update_file):
+        with open(last_update_file, "r") as f:
+            last_update = int(f.readline())
+        server_files = [f for f in server_files if int(f.split(".")[1][:8]) > last_update]
+
+    log.info("Processing {} files.".format(len(server_files)))
+
+    for server_file in server_files:
+        log.info("Downloading file {}.".format(server_file), indent=1)
+        temp_file = os.path.join(parent, server_file + ".temp")
+        try:
+            conn.get(os.path.join(folder, server_file), temp_file)
+            df = pd.read_csv(temp_file, sep=";")
+            df["time"] = pd.to_datetime(df['Date'], format='%Y%m%d%H', utc=True)
+            for station in df["Station/Location"].unique():
+                log.info("Processing station {}.".format(station), indent=2)
+                if not os.path.exists(os.path.join(parent, station)):
+                    os.makedirs(os.path.join(parent, station))
+                station_data = df.loc[df['Station/Location'] == station]
+                for day in pd.date_range(start=df['time'].min(), end=df['time'].max(), freq='D'):
+                    station_day_file = os.path.join(parent, station, "VQCA44.{}.csv".format(day.date().strftime('%Y%m%d')))
+                    station_day_data = station_data[station_data['time'].dt.date == day.date()].drop('time', axis=1)
+                    if not os.path.exists(station_day_file) or len(station_day_data) == 24:
+                        log.info("Saving file {}.".format(station_day_file), indent=3)
+                        station_day_data.to_csv(station_day_file)
+            if os.path.exists(temp_file):
+                os.unlink(temp_file)
+        except:
+            log.error("Failed to download {}.".format(server_file))
+            if os.path.exists(temp_file):
+                os.unlink(temp_file)
+            failed.append(server_file)
+
+    with open(last_update_file, "w") as f:
+        f.write(server_files[-1].split(".")[1][:8])
+
+    log.info("Closing connection to {}".format(ftp_host))
+    conn.close()
+
+    if len(failed) > 0:
+        raise ValueError("Failed to download and process: {}".format(", ".join(failed)))
+
